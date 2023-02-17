d23f991e8ce0e766e2aed1a05d027b6497295413
Bruno Rodrigues Faria <80405594+brunofaria27@users.noreply.github.com>
2023-02-17 19:33:35 -0300

    layout button - falta table

--- to-do-app/src/core/List.tsx
+++ to-do-app/src/core/List.tsx
@@ -4,8 +4,33 @@ import { Button, Typography } from "@mui/material";
 
 export const List = () => {
   return (
-    <div>
-        CSS FDP
+    <div
+      style={{
+        padding: 30,
+      }}
+    >
+      <Typography
+        variant="caption"
+        color="white"
+        component="div"
+        textAlign="center"
+      >
+        Crie tarefas, remova e edite para uma melhor organização do seu dia a
+        dia.
+      </Typography>
+      <div style={{ display: "flex", alignItems: "center", paddingTop: 30 }}>
+        <Typography
+          variant="h6"
+          color="white"
+          component="div"
+          padding="right"
+          flex={1}
+        >
+          LISTA DE TAREFAS
+        </Typography>
+
+        <Button variant="contained">Adicionar</Button>
+      </div>
     </div>
   );
 };
